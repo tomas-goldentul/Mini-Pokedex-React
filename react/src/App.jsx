124c55f37fe50557a7fada9f9f1af5640d1cfcd4
Android Studio <devandroid@ort>
2026-04-20 15:53:15 -0300

    Funcionamiento realizado, falta ajustar los tipos

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import './App.css';
 import IngresarNombre from './ingresarNombre.jsx';
 
 function App() {
@@ -21,7 +20,13 @@ function App() {
       <IngresarNombre onNombreChange={setNombre} />
       {data && (
         <div>
-          <p>Nombre: {data.name} <br /> Imagen: {data.sprites.front_default}</p>
+          <p>Nombre: {data.name}</p>
+          <p>ID: {data.id}</p>
+          <img src={data.sprites.other["official-artwork"].front_default} alt={data.name} />
+          <p>Tipo/s: {data.types[0].type.name}</p>
+          <p>Peso: {data.weight / 10} kg</p>
+          <p>Altura: {data.height / 10} m</p>
+
         </div>
       )}
     </>
--- react/src/ingresarNombre.jsx
+++ react/src/ingresarNombre.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import './ingresarNombre.css'
 
 function IngresarNombre({ onNombreChange }) {
   const [nombre, setNombre] = useState('');
