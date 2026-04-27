1608cfa4cc36e1380e34ace9072c3df5f7699d6b
TOMAS ARIEL GOLDENTUL <49319223@ort>
2026-04-27 14:24:03 -0300

    Ahora manejamos los errores

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -11,30 +11,41 @@ function App() {
       fetch(`https://pokeapi.co/api/v2/pokemon/${nombre.toLowerCase()}/`)
         .then(res => res.json())
         .then(json => setData(json))
-        .catch(err => setData(null) );
-    }
+        .catch(err => setData(null  ) );
+    } 
   }, [nombre]);
   
-  return(
-    <>
+  return (
+  <>
     <h1>Pokedex</h1>
-      <img src="https://fontmeme.com/permalink/260422/95136b1b.png" alt=""/>
-      <IngresarNombre onNombreChange={setNombre} />
-      {data && (
+    <img
+      src="https://fontmeme.com/permalink/260422/95136b1b.png"
+      alt=""
+    />
+    <IngresarNombre onNombreChange={setNombre} />
 
-        <div class = "Display_Pokemon">
-          <div>
+    {data ? (
+      <div className="Display_Pokemon">
+        <div>
           <p>Nombre: {data.name}</p>
           <p>ID: {data.id}</p>
-          <img src={data.sprites.other["official-artwork"].front_default} alt={data.name} />
-          <p>Tipo/s: {data.types[0].type.name} {data.types[1] && data.types[1].type.name}</p>
-          <p>Peso: {data.weight / 10} kg</p>
-          <p>Altura: {data.height / 10} m</p>
-          </div>
+          <img
+            src={data.sprites.other["official-artwork"].front_default}
+            alt={data.name}
+          />
+          <p>
+            Tipo/s: {data.types[0].type.name}{" "}
+            {data.types[1] && data.types[1].type.name}
+          </p>
+          <p>🏋️Peso: {data.weight / 10} kg</p>
+          <p>📏Altura: {data.height / 10} m</p>
         </div>
-      )}
-      
-    </>
-  );
+      </div>
+    ) : (
+     nombre && <p style={{ color: "red" }}>Error: no se encontraron datos del Pokémon</p>
+    )}
+  </>
+);
+
 }
 export default App;
